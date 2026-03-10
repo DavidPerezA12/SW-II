1730ffa60ebaf9594a5a3c6dbd45a9e3155f1220
alvaro-sanzc <76051058+alvaro-sanzc@users.noreply.github.com>
2026-03-10 17:28:36 +0100

    Agregado código de conexión a MongoDB

--- api/db/conn.js
+++ api/db/conn.js
@@ -1,1 +1,27 @@
-// CONECTARSE A MONGODB
+require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
+const { MongoClient } = require("mongodb"); // Importar el módulo cliente de MongoDB
+
+const client = new MongoClient(process.env.MONGODB_URI); // Instancia del cliente de MongoDB
+let dbConnection;
+
+// Función para conectar a la base de datos
+connectToDatabase = async () => {
+    try {
+        await client.connect();
+        dbConnection = client.db();
+        console.log("Succesfully connected to database");
+    } catch (e){
+        console.error(e);
+        process.exit();
+    }
+};
+
+// Función para obtener la conexión a la base de datos
+getDb = () => {
+    return dbConnection;
+}
+
+module.exports = {
+    connectToDatabase,
+    dbConnection
+};
