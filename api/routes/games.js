9873e065dc231ddacce6e01960c553cd6b63f5b1
alvaro-sanzc <76051058+alvaro-sanzc@users.noreply.github.com>
2026-03-12 15:27:40 +0100

    Actualiza la ruta de juegos para obtener datos desde la API RAWG

--- api/routes/games.js
+++ api/routes/games.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const mongodb = require(`../db/conn`);
+const rawg  = require(`../services/rawg`);
 
 // Obtener todos los juegos
 router.get('/', async (req, res) => {
     try {
-        const dbConnect = mongodb.getDb();
-        const games = await dbConnect.collection('games').find({}).toArray();
+        //const dbConnect = mongodb.getDb();
+        const games = await rawg.getGames(1);
+        console.log("Games:", games);
         res.json(games);
     } catch (err) {
         res.status(500).json({ message: 'Error fetching games', error: err });
